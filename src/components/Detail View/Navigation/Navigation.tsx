f4bb1354ea1aff162f7efdcbb98f54332b2eedd3
chris3evans <chris3evans.ce@gmail.com>
2025-08-30 11:02:11 +0100

    [Feature] - moved navigation into own component

---
+++ src/components/Detail View/Navigation/Navigation.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import styles from "./Navigation.module.scss";
+import { Svg } from "../../Svg/Svg";
+
+export function Navigation() {
+  return (
+    <div className={styles["navigation"]}>
+      <Link to="/">
+        <Svg svgFill="#ffffff" svgName="leftArrow"></Svg>
+      </Link>
+      <div className={styles["day-navigation"]}>
+        <h2>Saturday, 30th August 2025</h2>
+        <button className={styles["day-navigation-button"]}>
+          <Svg svgFill="#ffffff" svgName="leftArrow"></Svg>
+          <p>Previous Day</p>
+        </button>
+        <button className={styles["day-navigation-button"]}>
+          <p>Next Day</p> <Svg svgFill="#ffffff" svgName="rightArrow"></Svg>
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/views/DetailView/DetailView.tsx
+++ src/views/DetailView/DetailView.tsx
@@ -1,25 +1,10 @@
-import { Link } from "react-router-dom";
-import { Svg } from "../../components/Svg/Svg";
 import styles from "./DetailView.module.scss";
+import { Navigation } from "../../components/Detail View/Navigation/Navigation";
 
 export function DetailView() {
   return (
     <div className={styles["detail-view"]}>
-      <div className={styles["navigation"]}>
-        <Link to="/">
-          <Svg svgFill="#ffffff" svgName="leftArrow"></Svg>
-        </Link>
-        <div className={styles["day-navigation"]}>
-          <h2>Saturday, 30th August 2025</h2>
-          <button className={styles["day-navigation-button"]}>
-            <Svg svgFill="#ffffff" svgName="leftArrow"></Svg>
-            <p>Previous Day</p>
-          </button>
-          <button className={styles["day-navigation-button"]}>
-            <p>Next Day</p> <Svg svgFill="#ffffff" svgName="rightArrow"></Svg>
-          </button>
-        </div>
-      </div>
+      <Navigation />
       <div className={styles["hourly-weather-section"]}></div>
       <div className={styles["weather-metrics"]}></div>
     </div>
